bc76028b5d6a4f4fff0a1cdef2de0ab8dca8b81d
ChicagoADM <6974420@gmail.com>
2022-10-17 12:03:38 +0300

    Task_64

---
+++ Task_64/Program.cs
@@ -0,0 +1,28 @@
+﻿void Exit()
+{
+    Console.WriteLine();
+    Console.Write("Нажмите любою клавишу... ");
+    Console.ReadKey();
+}
+//Задача 64: Задайте значения M и N. Напишите рекурсивный метод, который выведет все натуральные числа кратные 3-ём в промежутке от M до N.
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Натуральные числа кратные 3-ём: ");
+Zadacha64(n,m);
+void Zadacha64(int n, int m)
+{
+    {
+        m++;
+        if (m % 3 == 0)
+            Console.Write(m + " ");
+        if (m >= n)
+            return;
+    }
+    Zadacha64(n,m);
+}
+
+Exit();
